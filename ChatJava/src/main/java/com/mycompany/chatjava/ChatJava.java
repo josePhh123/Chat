8fb93faa7d69d8c098f3d45dbe75e01e191eb977
Joseph <joseph.guadalupe@unmsm.edu.pe>
2024-03-05 23:00:10 -0500

    Crear Carpetas

--- ChatJava/src/main/java/com/mycompany/chatjava/ChatJava.java
+++ ChatJava/src/main/java/com/mycompany/chatjava/ChatJava.java
@@ -11,6 +11,6 @@ package com.mycompany.chatjava;
 public class ChatJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello git hub!");
+        System.out.println("Hello git hubt!");
     }
 }
